d74afeb32e82463a5574b6c069516f7e7f14e018
Ross Wu <RAWSE-DA-BAWSE@Rosss-MacBook-Pro.local>
2017-05-18 19:32:08 -1000

    done

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,20 @@
 'use strict';
 module.exports = function() {
-  
+
+var secretNumber = (function (){
+    var secretNumberGenerator = Math.floor(Math.random()*(100000 - 0 )) +0;
+
+    return function (){
+        if(secretNumberGenerator === secretNumberGenerator){
+
+            return secretNumberGenerator;
+
+        };
+
+    };
+    return secretNumberGenerator ++;
+
+})(); // end of first function
+  return secretNumber;  
+
 };
